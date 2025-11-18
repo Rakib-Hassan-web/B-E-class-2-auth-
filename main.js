3bf49afed05090bd7e88bffb16aa33e81a4cf4a9
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-11-18 20:48:23 +0600

    Regster confirm

--- main.js
+++ main.js
@@ -1,67 +1,54 @@
  const express = require('express');
-
-
-
  const app =express();
  app.use(express.json())
 
 
 app.get ('/' ,(req ,res )=>{
-    res.send('Hello world')
+    res.send('This is home page')
 })
 
+// ----------------------Login Part---------------
 
 app.get ('/login' ,(req ,res )=>{
 
     const {Email , Password}=req.body;
-
-    console.log(Email)
-    console.log(Password)
-
-
-    if(!Email) return res.send('Email is required')
-    if(!Password) return res.send('Password is required')
+   
+    if(!Email) {res.send('Email is required')
 
 
 
+        if(!Password) return res.send('Password is required')
 
 
-    res.send('Login successfull ')
+            else  { res.send('Login Successfull')}
+    }
+
+
+
+    res.send('This is login page ')
 
 })
 
 
+// ------------------------Register Part---------------------
 
-app.post('/register' ,(req ,res)=>{
-
-
+app.get('/register' ,(req ,res)=>{
 
     const { Name ,Email,Password,ConfirmPassword} =req.body
 
     console.log(req.body);
-    
 
     if(!Name) return res.send('Name is required')
     if(!Email) return res.send('Email is required')
     if(!Password) return res.send('Password is required')
     if(Password != ConfirmPassword) return res.send('Passwords do not match')
-    
     if(!ConfirmPassword) return res.send('Confirm Password is required')
-
-
-
-
 
     res.send('register Sucessfull')
 
 })
 
-
-
-
-
-
-
+// ------------------------Server Past-------------------
 
  app.listen(4000,()=>{
 
